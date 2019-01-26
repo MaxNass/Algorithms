0416006f5e93f649a22f3849cb5d2896a67950b3
MaxNass <MaxNass@users.noreply.github.com>
2019-01-26 15:35:41 +0100

    Add files via upload

---
+++ Algorithms/src/OtherAlgorithms/NombresPremiersAlgorithm.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package OtherAlgorithms;
+
+/**
+ * @author MaxNass
+ *
+ */
+public class NombresPremiersAlgorithm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+	
+	/**
+	 * 
+	 */
+	public void decouverteDesNombresPremiers () {
+		
+	}
+
+}
